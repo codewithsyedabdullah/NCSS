a6b7d968c0d751b5b4f9906752b3f8d2e0814687
Syed Abdullah Yaqoob <abdullah2684511@gmail.com>
2026-06-09 15:37:35 +0500

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    viteStaticCopy({
+      targets: [
+        { src: 'careers.html', dest: '.' },
+        { src: 'contacts.html', dest: '.' },
+        { src: 'ncssstories.html', dest: '.' },
+        { src: 'css/style.min.css', dest: '.' },
+        { src: 'css/tvcss-theme.css', dest: '.' },
+        { src: 'js/*', dest: '.' },
+        { src: 'img/**', dest: '.' },
+        { src: 'favicons/**', dest: '.' },
+      ],
+    }),
+  ],
 })
